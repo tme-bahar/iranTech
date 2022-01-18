73e6e8ab46ef2b9766cbfdcf13c4630b0c95a752
fun-programing <82670328+fun-programing@users.noreply.github.com>
2022-01-18 23:02:25 +0330

    sixth

--- lib/main.dart
+++ lib/main.dart
@@ -48,79 +48,29 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           child: Stack(
             children: [
+              //orange cube
               Positioned(top: 350,right: -120,
                     child:
-                  Transform.rotate(angle: -3.14 / 4, child:
-                      SizedBox(width: 330,height: 320,child:
-                        Container(
-                          decoration: const BoxDecoration(color: back,borderRadius: BorderRadius.all(Radius.circular(40))
-                            ,boxShadow: [
-                              BoxShadow(
-                                 color: orange,
-                                 spreadRadius: -30,
-                                 blurRadius: 40,
-                                 offset: Offset(-15, -15), // changes position of shadow
-                              )
-                            ]),
-                        )
-                      )
+                  Transform.rotate(angle: -3.14 / 4, child:BackCube(shadow: orange,back: back,offset: const Offset(-15,-15),)
                   )
                 ),
+              //blue cube
               Positioned(top: 175,left: -120,
                   child:
-                  Transform.rotate(angle: -3.14 / 4, child:
-                    SizedBox(width: 320,height: 320,child:
-                      Container(
-                        decoration: const BoxDecoration(color: back,borderRadius: BorderRadius.all(Radius.circular(40))
-                            ,boxShadow: [
-                              BoxShadow(
-                                color: blue,
-                                spreadRadius: -30,
-                                blurRadius: 40,
-                                offset: Offset(15, 15), // changes position of shadow
-                              )
-                            ]),
-                      )
-                    )
-                  )
+                  Transform.rotate(angle: -3.14 / 4, child:BackCube(shadow: blue,back: back,offset: const Offset(15,15),))
               ),
+              //logo
               Center(child: SizedBox(width: 260,child: Image.asset('assets/logo.png'),)),
-              Align(alignment: Alignment.bottomCenter,child:
-                Container(
-                  margin: const EdgeInsets.only(bottom: 100),child:
-                    SizedBox(width: 240,height: 50,child:
-                      ElevatedButton(onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Login()));
-                      }, child: const Text('ورود',style: TextStyle(fontSize: 18,color: Colors.black)),
-                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(blue),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25.0),
-                            ),
-                          ),
-                        ),
-                      )
-                    )
-                ),
+              //login button
+              Button(color: blue,margin: 100,textColor: Colors.black,text: 'ورود',onPressed:() {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => Login()));
+                } ,
               ),
-              Align(alignment: Alignment.bottomCenter,child:
-                Container(
-                    margin: const EdgeInsets.only(bottom: 170),child:
-                    SizedBox(width: 240,height: 50,child:
-                      ElevatedButton(onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUp()));
-                      }, child: const Text('ثبت نام',style: TextStyle(fontSize: 18,color: Colors.white)),
-                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(orange),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                            ),
-                          ),
-                        ),
-                      )
-                    )
-                ),
-              )
+              //sign up button
+              Button(color: orange,margin: 160,textColor: Colors.white,text: 'ثبت نام',onPressed:() {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUp()));
+              },
+              ),
             ],
       ),
       ),
@@ -146,58 +96,12 @@ class Login extends StatelessWidget {
               transform: GradientRotation(1.57))
           ),
           child: Stack(children: [
-            Align(alignment: Alignment.bottomCenter,child:
-              Container(
-                  margin: const EdgeInsets.only(bottom: 100),child:
-                SizedBox(width: 240,height: 50,child:
-                  ElevatedButton(onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => Login()));},
-                    child: const Text('ورود',style: TextStyle(fontSize: 18,color: Colors.black)),
-                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(blue),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                      ),
-                    ),
-                  )
-                )
-              ),
-            ),
-            Align(alignment: Alignment.bottomCenter,child:
-              Container(
-                  margin: const EdgeInsets.only(bottom: 320),child:
-                  const SizedBox(width: 350,child:
-                    TextField(decoration:
-                      InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2.0),
-                        ),
-                        hintText: 'رمز عبور',
-                        hintTextDirection: TextDirection.rtl,
-                        hintStyle: TextStyle(color: Colors.white)
-                      ),
-                    )
-                  )
-              ),
-            ),
-            Align(alignment: Alignment.bottomCenter,child:
-              Container(
-                  margin: const EdgeInsets.only(bottom: 400),child:
-                  const SizedBox(width: 350,child:
-                    TextField(decoration:
-                      InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey, width: 2.0),
-                          ),
-                          hintText: 'پست الکترونیکی',
-                          hintTextDirection: TextDirection.rtl,
-                          hintStyle: TextStyle(color: Colors.white)
-                      ),
-                    )
-                  )
-              ),
-            ),
+            //login button
+            Button(text:'ورود' ,margin: 100,color: blue,textColor: Colors.black,onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));},),
+            //password text filed
+            EditText(hint: 'رمز عبور', margin: 320),
+            EditText(hint: 'پست الکترونیکی', margin: 400),
             Align(alignment: Alignment.bottomCenter,child:
               Container(
                   margin: const EdgeInsets.only(bottom: 290),child:
@@ -230,7 +134,7 @@ class SignUp extends StatelessWidget {
                     transform: GradientRotation(1.57))
             ),
             child: Stack(children: [
-              Button(text: 'ثبت نام',color: orange,onPressed: () {
+              Button(text: 'ثبت نام',margin: 100,textColor: Colors.white,color: orange,onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(builder: (context) => Home()));},),
               EditText(margin : 480,hint : 'نام'),
               EditText(margin : 320,hint : 'شماره تلفن همراه'),
@@ -254,39 +158,88 @@ class Home extends StatelessWidget {
     const Color back = Color.fromARGB(255, 27, 40, 69);
     const Color orange = Color.fromARGB(255, 255, 74, 28);
     return Scaffold(
-        appBar: AppBar(actions:  const [ Align(alignment: Alignment.centerRight,
-            child: Padding(padding: EdgeInsets.only(right: 20),
-              child: Text('خانه',style: TextStyle(fontSize: 25),),)),],
-            automaticallyImplyLeading: false,
-            backgroundColor: back
-            ,title:Row(children: [
-              IconButton(icon:const Icon(Icons.menu), onPressed: () {  },),
-              IconButton(icon:const Icon(Icons.add), onPressed : (){Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddCentralDevice()));})],))
-        ,resizeToAvoidBottomInset: false,
-        body: Container(
-            alignment: Alignment.center,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(colors: [back, Colors.black],
-                    transform: GradientRotation(1.57))
-            ),
-            child: Stack(children: [
-              MaterialApp(
+        resizeToAvoidBottomInset: false,
+        body: MaterialApp(
                 home: DefaultTabController(
                   length: 3,
                   child: Scaffold(backgroundColor: const Color.fromARGB(0, 0, 0, 0) ,
                     bottomNavigationBar: menu(),
                     body: TabBarView(
                       children: [
-                        Container(child: Center(child: Text('موردی برای نمایش وجود ندارد',style: TextStyle(color: Colors.white),),)),
-                        Container(child: Icon(Icons.directions_transit)),
-                        Container(child: Icon(Icons.directions_bike)),
+                        Scaffold(appBar:
+                          AppBar(
+                              actions:  const [
+                                Align(alignment: Alignment.centerRight,
+                                  child: Padding(padding: EdgeInsets.only(right: 20),
+                                    child: Text('خانه',style: TextStyle(fontSize: 25),),)),],
+                              automaticallyImplyLeading: false,
+                              backgroundColor: back,
+                              title:
+                                Row(children: [
+                                  IconButton(icon:const Icon(Icons.menu), onPressed: () {  },),
+                                  IconButton(icon:const Icon(Icons.add), onPressed : (){Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddCentralDevice()));})],
+                              )
+                          ),
+                          body: Container(
+                              child:const Center(child: Text('موردی برای نمایش وجود ندارد',
+                              style: TextStyle(color: Colors.white),),),
+                              alignment: Alignment.center,
+                              decoration: const BoxDecoration(
+                                  gradient: LinearGradient(colors: [back, Colors.black],
+                                      transform: GradientRotation(1.57))
+                              ),),
+                        ),
+                        Scaffold(appBar:
+                          AppBar(
+                              actions:  const [
+                                Align(alignment: Alignment.centerRight,
+                                    child: Padding(padding: EdgeInsets.only(right: 20),
+                                      child: Text('خانه',style: TextStyle(fontSize: 25),),)),],
+                              automaticallyImplyLeading: false,
+                              backgroundColor: back,
+                              title:
+                              Row(children: [
+                                IconButton(icon:const Icon(Icons.menu), onPressed: () {  },),
+                                IconButton(icon:const Icon(Icons.add), onPressed : (){Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddCentralDevice()));})],
+                              )
+                          ),
+                          body: Container(
+                            child:const Center(child: Text('موردی برای نمایش وجود ندارد',
+                              style: TextStyle(color: Colors.white),),),
+                            alignment: Alignment.center,
+                            decoration: const BoxDecoration(
+                                gradient: LinearGradient(colors: [back, Colors.black],
+                                    transform: GradientRotation(1.57))
+                            ),),
+                        ),
+                        Scaffold(appBar:
+                          AppBar(
+                              actions:  const [
+                                Align(alignment: Alignment.centerRight,
+                                    child: Padding(padding: EdgeInsets.only(right: 20),
+                                      child: Text('خانه',style: TextStyle(fontSize: 25),),)),],
+                              automaticallyImplyLeading: false,
+                              backgroundColor: back,
+                              title:
+                              Row(children: [
+                                IconButton(icon:const Icon(Icons.menu), onPressed: () {  },),
+                                IconButton(icon:const Icon(Icons.add), onPressed : (){Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddCentralDevice()));})],
+                              )
+                          ),
+                          body: Container(
+                            child:const Center(child: Text('موردی برای نمایش وجود ندارد',
+                              style: TextStyle(color: Colors.white),),),
+                            alignment: Alignment.center,
+                            decoration: const BoxDecoration(
+                                gradient: LinearGradient(colors: [back, Colors.black],
+                                    transform: GradientRotation(1.57))
+                            ),),
+                        ),
                       ],
                     ),
                   ),
                 ),
               )
-            ])
-        )
     );
   }
   Widget menu() {
@@ -402,12 +355,12 @@ class EditText extends Align{
 }
 
 class Button extends Align{
-   Button({Key? key,String? text,Color? color,VoidCallback? onPressed}) : super(key: key,alignment:Alignment.bottomCenter,
+   Button({Key? key,required double margin,String? text,Color? textColor,Color? color,VoidCallback? onPressed}) : super(key: key,alignment:Alignment.bottomCenter,
       child:Container(
-          margin: const EdgeInsets.only(bottom: 100),child:
+          margin: EdgeInsets.only(bottom: margin),child:
         SizedBox(width: 240,height: 50,child:
           ElevatedButton(onPressed: onPressed,
-            child: Text(text!,style: TextStyle(fontSize: 18,color: Colors.white)),
+            child: Text(text!,style: TextStyle(fontSize: 18,color: textColor)),
             style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(color!),
               shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                 RoundedRectangleBorder(
@@ -419,5 +372,20 @@ class Button extends Align{
         )
       ),
    );
+}
+
+class BackCube extends SizedBox{
+  BackCube({Color? back,Color? shadow,Offset? offset}) : super(width: 320,height: 320,
+      child:Container(
+        decoration:  BoxDecoration(color: back,borderRadius: BorderRadius.all(Radius.circular(40))
+            ,boxShadow: [
+              BoxShadow(
+                color: shadow!,
+                spreadRadius: -30,
+                blurRadius: 40,
+                offset: offset!, // changes position of shadow
+              )
+            ]),
+      ) );
 
 }
